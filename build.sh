fb512d4b7bdca28cd70b39165c2a769efbddcae4
archification <archification@gmail.com>
2026-05-01 14:35:11 -0400

    40% default volume and added xwin command to build script for compiling to windows

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 RUSTFLAGS="-Zlocation-detail=none" cross build --target x86_64-unknown-linux-musl --release
+cargo xwin build --release --target x86_64-pc-windows-msvc
 
 upx --best --lzma target/x86_64-unknown-linux-musl/release/rsbpm
+strip target/x86_64-pc-windows-msvc/release/rsbpm.exe
+upx --best --lzma target/x86_64-pc-windows-msvc/release/rsbpm.exe
